71b83f36b27ed1f9308c9583b9e7cba11248d040
SlaYpoint <66507607+SlaYpoint@users.noreply.github.com>
2021-08-09 23:12:42 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,14 @@
+const date = document.querySelector("input[type=date]");
+const reset = document.querySelector("#reset");
+const accept = document.querySelector("#hide");
+
+// To reset/clear values
+// reset.addEventListener("click", () => {
+//   date.value = null;
+//   document.querySelector(".message").innerText = "";
+// });
+
+// // Hide pop-up
+// accept.addEventListener("click", (e) => {
+//   e.target.parentElement.style.display = "none";
+// });
